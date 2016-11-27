9910e10c1792b7348dea0c02502dd67c4b188bc1
Jose Ordaz <joseordaz@Joses-MacBook-Pro.local>
2016-11-27 15:46:48 -0600

    Done

--- src/app/custom-counter.component.ts
+++ src/app/custom-counter.component.ts
@@ -2,54 +2,10 @@ import { Component, Input, forwardRef, Output } from '@angular/core';
 
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-
- /* <input 
-    type="checkbox" 
-    value="1"
-    id="{{identifier || 'styled-checkbox'}}"
-    class="styled"
-    [checked]="checked"
-    (change)="checked=$event.target.checked"
-    />
-  <label 
-    tabindex=0
-    (keydown)="onKeyDown($event)"
-    htmlFor="{{identifier || 'styled-checkbox'}}">
-    <span></span>
-    <ng-content></ng-content>
-  </label>*/
-
 @Component({
     selector: 'styled-checkbox',
     template: `
-
         <style>
-            label {
-                font-weight: 600;
-                font-size: 11px;
-                margin-bottom: 0px;
-            }
-
-            input.styled[type="checkbox"] {
-                display:none;
-            }
-
-            input.styled[type="checkbox"] + label span {
-                display:inline-block;
-                width:28px;
-                height:28px; 
-                margin:-1px 4px 0 0;
-                vertical-align:middle;
-                background:url('./img/checkbox_unchecked.png') left top no-repeat;
-                cursor:pointer;
-                background-size: cover;
-            }
-
-            input.styled[type="checkbox"]:checked + label span {
-                background:url('./img/checkbox_checked.png') left top no-repeat;
-                background-size: cover;
-            }
-
             .tmpOn {
                 background:url('./img/checkbox_checked.png') left top no-repeat;
                 background-size: cover;
@@ -81,31 +37,40 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
             }
 
             .disabled {
+                background:url('./img/checkbox_unchecked.png') left top no-repeat;
+                background-size: cover;
+                font-weight: 600;
+                font-size: 11px;
+                margin-bottom: 0px;
+                margin:-1px 4px 0 0;
+                vertical-align:middle;
+                display:inline-block;
+                width:28px;
+                height:28px;
+                cursor:default;
+                background-size: cover;
+            }
+
+            .textDisabled {
                 text-decoration: none;
+                color : lightgrey ;
+                cursor:default;
             }
-
-            .disabled:hover {
-                cursor : default;
-            }
+            
         </style>
-
-         <span 
-            [ngClass] = "
-                {
-                    'disabled':disabled
-                }"
-        >
-
-        <span 
-            [ngClass] = "
-                {
-                    'tmpOn':isChecked,
-                    'tmpOff':!isChecked
-                }" 
-            (click) = 'changeValue()'
-        ></span>
-            {{text}}
-        </span>
+        
+            <span 
+                [ngClass] = "
+                    {
+                        'tmpOn':isChecked && !disabled,
+                        'tmpOff':!isChecked && !disabled,
+                        'disabled':disabled
+                    }" 
+                (click) = 'changeValue()'
+            ></span>
+            <span [ngClass]="{'textDisabled':disabled}">
+                <ng-content></ng-content>
+            </span>
 
     `,
     providers:[
@@ -119,8 +84,6 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 export class CounterInputComponent implements ControlValueAccessor{
 
     isChecked = false;
-
-    @Input() text:string;
 
     @Input() disabled: boolean;
 
@@ -144,5 +107,4 @@ export class CounterInputComponent implements ControlValueAccessor{
             this.propagateChange(this.isChecked);
         }
     }
-
 }
